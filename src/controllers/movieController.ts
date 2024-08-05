c4d1722ae003f48ff7bfc344e780bea0636a0315
sgayathrii <sgayathrii@gmail.com>
2024-08-05 21:27:39 +0200

    Pagination support added to list movies end point

--- src/controllers/movieController.ts
+++ src/controllers/movieController.ts
@@ -3,8 +3,20 @@ import { getMovies } from "../services/movieService";
 
 export const listMovies = async (req: Request, res: Response) => {
     try {
-        const movies = await getMovies();
-        res.json({ data: movies });
+        const page = parseInt(req.query.page as string, 10) || 1;
+        const pageSize = parseInt(req.query.pageSize as string, 10) || 10;
+
+        const { movies, totalCount } = await getMovies(page, pageSize);
+        
+        res.json({
+            data: movies,
+            pagination: {
+                page,
+                pageSize,
+                totalCount,
+                totalPages: Math.ceil(totalCount / pageSize),
+            },
+        });
     } catch(error) {
         res.status(500).json({ error: 'An error occurred while fetching movies'+error });
     }
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,9 +1,21 @@
 import prisma from "../../prisma";
 
-export const getMovies = async () => {
+export const getMovies = async (page: number, pageSize: number) => {
     try {
-        const movies = await prisma.movie.findMany();
-        return movies;
+
+        const skip = (page - 1) * pageSize;
+
+        const movies = await prisma.movie.findMany({
+            skip,
+            take: pageSize, 
+        });
+
+        const totalCount = await prisma.movie.count();
+
+        return {
+            movies,
+            totalCount,
+        };
     } catch (error) {
         console.error('Error fetching movies:', error);
         throw new Error('Failed to fetch movies');
