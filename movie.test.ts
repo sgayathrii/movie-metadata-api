53c6d1905a47cd26238872092f7c18f72b0cb365
sgayathrii <sgayathrii@gmail.com>
2024-08-06 20:45:26 +0200

    Added test to check endpoints

---
+++ movie.test.ts
@@ -0,0 +1,30 @@
+import request from "supertest";
+import app from "./src/app";
+
+describe("Movie API Endpoints", () => {
+  describe("GET /api/movies", () => {
+    it("should return a list of movies", async () => {
+      const response = await request(app).get("/api/movies");
+
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty("data");
+      expect(response.body).toHaveProperty("pagination");
+      expect(Array.isArray(response.body.data)).toBe(true);
+    });
+  });
+});
+
+describe('PUT /api/movies/:id', () => {
+    it('should update a movie', async () => {
+      const movieId = 2510; 
+      const updatedData = { title: 'Updated Movie Title' };
+
+      const response = await request(app)
+        .put(`/api/movies/${movieId}`)
+        .send(updatedData);
+
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty('data');
+      expect(response.body.data.title).toBe(updatedData.title);
+    });
+});
